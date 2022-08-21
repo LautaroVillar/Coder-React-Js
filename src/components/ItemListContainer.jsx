ab652b82d827e7706049354e4999b6a0ae51c46a
Lautaro Villar <lautarovillar@outlook.com>
2022-08-21 13:15:34 -0300

    segunda y tercer entrega

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ItemListContainer() {
+  return (
+    <div>
+        <h1>E-COMMERCE</h1>
+        <hr />
+        <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod est vero neque numquam? Dolores sapiente quibusdam iure similique nulla error nesciunt inventore, vitae ullam earum reprehenderit ad ipsum, ex ipsam.</h3>
+        <hr />
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ex deleniti pariatur asperiores? Provident sint, iste voluptates nostrum numquam aliquam mollitia repellendus perspiciatis maiores. Nisi ratione vel necessitatibus quidem hic.</h4>
+        <hr />
+        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam alias error, repellendus nam amet asperiores beatae similique, repudiandae officiis explicabo iusto eum labore voluptatem laborum consequuntur autem, magnam quam? Velit.</h5>
+    </div>
+  )
+}
